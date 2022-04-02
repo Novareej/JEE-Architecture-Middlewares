9fb634f23dc55463d175c0bca7c1bf967e8e317f
Arij Bouhlala <Bouhlalaarij@gmail.com>
2022-04-02 19:45:11 +0000

    new activity jpa

---
+++ jpa-users-roles/src/main/java/ma/enset/jpausersroles/JpaUsersRolesApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.jpausersroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUsersRolesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaUsersRolesApplication.class, args);
+    }
+
+}
