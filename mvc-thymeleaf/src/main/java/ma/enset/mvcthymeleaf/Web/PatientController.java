162d42aeb3b31e25ac1c5135096f619642bc71fc
Arij Bouhlala <Bouhlalaarij@gmail.com>
2022-04-24 16:20:17 +0000

    Pagination

--- mvc-thymeleaf/src/main/java/ma/enset/mvcthymeleaf/Web/PatientController.java
+++ mvc-thymeleaf/src/main/java/ma/enset/mvcthymeleaf/Web/PatientController.java
@@ -3,9 +3,12 @@ package ma.enset.mvcthymeleaf.Web;
 import lombok.AllArgsConstructor;
 import ma.enset.mvcthymeleaf.Repositories.PatientRepository;
 import ma.enset.mvcthymeleaf.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,13 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping(path="/index")
-    public String patient(Model model){
-        List<Patient> patients=patientRepository.findAll() ;
-        model.addAttribute("ListPatients",patients);
+    public String patient(Model model,
+                          @RequestParam(name="page",defaultValue = "0") int page,
+                          @RequestParam(name="size",defaultValue = "5") int size){
+        Page<Patient> pagePatients=patientRepository.findAll(PageRequest.of(page,size)) ;
+        model.addAttribute("ListPatients",pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
         return "patients"; //View based on thymeleaf (resources/templates)
     }
 }
